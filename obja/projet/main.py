508e61dfba96a2f7b510216d6046f6059211f12f
NoxFelis <roselaminimoy@gmail.com>
2022-10-05 14:55:14 +0200

    update main

--- obja/projet/main.py
+++ obja/projet/main.py
@@ -3,6 +3,22 @@ import numpy as np
 import sys
 
 def main():
+	# 1 - On récupère l'objet dans son maillage irrégulier 
+	
+
+	# 2 - On crée le maillage de base 
+	# pour l'instant cette étape n'est pas gérée, on doit le faire à la main
+	# et on utilisera un code fourni
+
+	# 3 - On partitionne le maillage d'origine
+	# sur le maillage de base (patchs)
+
+	# 4 - On projète les patchs sur le maillage de base
+
+	# 5 - On travaille la subdivision
+
+	# on retourne le maillage semi-régulier
+
 	return 1
 
 if __name__ == '__main__':
